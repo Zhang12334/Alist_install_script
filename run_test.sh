12fd536c2deba46ccf83f11b587e2ae3ffb3ec29
Zhang1233 <105694975+Zhang12334@users.noreply.github.com>
2024-10-03 19:44:54 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+#本版本为未经测试的beta版本，非必要请勿使用
 # 下载地址
-download_url="Zhang12334/Alist_install_bash/releases/latest/download/install.sh"
+# download_url="Zhang12334/Alist_install_bash/releases/latest/download/install.sh"
+download_url="https://raw.githubusercontent.com/Zhang12334/Alist_install_bash/refs/heads/main/run_test.sh"
 test_file_url="https://github.com/Zhang12334/test_ghmirror/releases/latest/download/test.txt"
 
 # 清屏
@@ -19,28 +20,28 @@ read -p "请输入你的选择: " choice1
 
 # 根据用户选择设置代理
 case $choice1 in
-    1) useproxy="https://ghp.ci/https://github.com/" ;;
-    2) useproxy="https://ghproxy.net/https://github.com/" ;;
-    3) useproxy="https://ghproxy.cn/https://github.com/" ;;
-    4) useproxy="https://github.moeyy.xyz/https://github.com/" ;;
-    5) useproxy="https://gh.llkk.cc/https://github.com/" ;;
+    1) useproxy="https://ghp.ci/" ;;
+    2) useproxy="https://ghproxy.net/" ;;
+    3) useproxy="https://ghproxy.cn/" ;;
+    4) useproxy="https://github.moeyy.xyz/" ;;
+    5) useproxy="https://gh.llkk.cc/" ;;
     6) 
         read -p "请输入自定义加速地址: " custom_proxy
         # 验证自定义加速地址是否有效
         if curl -fsSL "${custom_proxy}${test_file_url}" | grep -q "114514"; then
             rm -f test.txt
-            useproxy="${custom_proxy}https://github.com/"
+            useproxy="${custom_proxy}"
             echo "已选择自定义加速地址: $custom_proxy"
         else
             rm -f test.txt
             echo "自定义加速地址无效，自动使用https://ghp.ci/加速地址"
-            useproxy="https://ghp.ci/https://github.com/" 
+            useproxy="https://ghp.ci/" 
         fi
         ;;
     7) useproxy="https://github.com/" ;;
     *) 
         echo "无效的选择，默认使用https://ghp.ci/加速地址"
-        useproxy="https://ghp.ci/https://github.com/" 
+        useproxy="https://ghp.ci/" 
         ;;
 esac
 echo
