b4e8bb7b2ebe08ff4207f489a0c728079cc7474a
Zhang1233 <105694975+Zhang12334@users.noreply.github.com>
2024-10-03 13:08:54 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
-# 使用GitHub API获取最新的发行版信息（里边有最新的版本号标识）
 response=$(curl -s https://api.github.com/repos/Zhang12334/Alist_install_bash/releases/latest)
-
-# 提取下载链接 （tag_name属性即为版本号属性 cut命令获取版本号属性的值 awk命令把版本号中的v字符删除）
 version=$(echo "$response" | grep 'tag_name' | cut -d'"' -f4 | awk '{print substr($0, 2, length($0) - 1)'})
-
-# 拼接下载连接
 download_url=https://github.com/Zhang12334/Alist_install_bash/releases/download/v$version/install.sh
-
-# 使用curl下载最新release脚本
-curl -fsSL -L "$download_url" | bash -s install
-
-#借鉴于：https://www.cnblogs.com/dingshaohua/p/17551097.html
+clear
+echo "请选择操作："
+echo "1. 安装"
+echo "2. 卸载"
+echo "3. 升级"
+echo -n "请输入你的选择: "
+read choice
+if [ "$choice" -eq 1 ]; then
+    curl -fsSL -L "$download_url" | bash -s install
+elif [ "$choice" -eq 2 ]; then
+    curl -fsSL -L "$download_url" | bash -s uninstall
+elif [ "$choice" -eq 3 ]; then
+    curl -fsSL -L "$download_url" | bash -s update
+else
+    echo "无效的选择，请输入1-3的数字"
+fi
+#部分借鉴于：https://www.cnblogs.com/dingshaohua/p/17551097.html
